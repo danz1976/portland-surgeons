fa47786e94a685e292ae5b36ef2eae8615eb320f
Daniel Zink <zink@Daniels-MacBook-Air.local>
2012-11-11 09:36:16 -0800

    updated karens writeup

--- zink.php
+++ zink.php
@@ -4,22 +4,21 @@
 
 <h2>Karen Zink, MD</h2>
 
-<p>
-Dr. Zink has recently completed her General Surgery Residency training at Oregon Health
-&amp; Sciences University. She earned her medical degree at Washington University School of
-Medicine in St. Louis. She completed her undergraduate degree at the University of Illinois at
-Urbana-Champaign.
-</p>
+<p>Dr. Zink completed her General Surgery Residency training at Oregon Health &amp;
+Sciences University in 2010. She is board certified by the American Board of
+Surgery and is a Fellow of the American College of Surgeons.  She earned her
+medical degree at Washington University School of Medicine in St. Louis. She
+completed her undergraduate degree at the University of Illinois at
+Urbana-Champaign.</p>
 
-<p>
-She is a general surgeon who is interested in the broad range of general surgery, with a particular
-interest in minimally invasive surgery. Areas of interest include gallbladder surgery, hernias
-(laparoscopic and open), colon surgery, surgery for acid reflux, and surgery for breast disease.
-</p>
+<p>She is a general surgeon who is interested in the broad range of general
+surgery, with a particular interest in minimally invasive surgery. Areas of
+interest include gallbladder surgery, hernias (laparoscopic and open), colon
+surgery, surgery for acid reflux, surgery for melanoma, and surgery for breast
+disease.</p>
 
-<p>
-When not working, she enjoys skiing, kayaking, juggling, unicycling, cooking, and travelling.
-</p>
+<p>When not working, she enjoys skiing, kayaking, juggling, unicycling,
+cooking, and traveling.</p>
 
 <a href="zink-cv.php">See Dr. Zink's CV</a>
 
