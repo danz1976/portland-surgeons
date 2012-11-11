4b845db8ca61604be4c90ad92caac924c6c69978
Daniel Zink <zink@Daniels-MacBook-Air.local>
2012-11-11 09:43:37 -0800

    changed kelvins picture

--- yu.php
+++ yu.php
@@ -1,24 +1,24 @@
 <?php include('header.php') ?>
 
-<img src="images/eKelvin3.jpg" width="217" height="300" class="face">
+<img src="images/kelvin4.jpg" width="217" height="300" class="face">
 
 <h2>Kelvin Yu, M.D.</h2>
 
 <p>
-Dr. Yu has been in surgical practice in Portland, Oregon since 1997. He 
-graduated from the University of California San Francisco School of Medicine 
-where he also started and completed residency training in General Surgery. He 
-also completed a fellowship in Surgical Oncology at the National Cancer 
-Institute, National Institutes of Health. He is board certified by the American 
+Dr. Yu has been in surgical practice in Portland, Oregon since 1997. He
+graduated from the University of California San Francisco School of Medicine
+where he also started and completed residency training in General Surgery. He
+also completed a fellowship in Surgical Oncology at the National Cancer
+Institute, National Institutes of Health. He is board certified by the American
 Board of Surgery and a fellow of the American College of Surgeons.
 </p>
 
 <p>
-His practice interest includes all aspects of General Surgery. As an Endocrine 
-Surgeon, he has a strong interest and expertise in the management and treatment 
-of Thyroid and Parathyroid disorders. As a Surgical Oncologist he has a 
-particular interest and proficiency in the management and treatment of 
-melanoma, breast disorders, and intra-abdominal malignancies and soft tissue 
+His practice interest includes all aspects of General Surgery. As an Endocrine
+Surgeon, he has a strong interest and expertise in the management and treatment
+of Thyroid and Parathyroid disorders. As a Surgical Oncologist he has a
+particular interest and proficiency in the management and treatment of
+melanoma, breast disorders, and intra-abdominal malignancies and soft tissue
 tumors.
 </p>
 
