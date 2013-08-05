ff765161d39986c9b0ab9524f0de62c9e7ebe8c8
Karen Zink <unspecified>
2013-08-04 21:17:46 -0700

    new_forms

--- forms.php
+++ forms.php
@@ -4,11 +4,12 @@
 
 <ul>
   <li><a href="forms/HIPAA.pdf">HIPAA.pdf</a>
-  <li><a href="forms/PS_FEMALE_HEALTH_SURVEY.pdf">PS FEMALE HEALTH SURVEY.pdf</a>
-  <li><a href="forms/PS_Medical_History.pdf">PS Medical History.pdf</a>
-  <li><a href="forms/PS_Patient_Registration.pdf">PS Patient Registration.pdf</a>
-  <li><a href="forms/PS_Payment_Policy.pdf">PS Payment Policy.pdf</a>
-  <li><a href="forms/PS_Review_of_Systems.pdf">PS Review of Systems.pdf</a>
+  <li><a href="forms/PS_Authorization.pdf">Authorization.pdf</a>
+  <li><a href="forms/PS_FEMALE_HEALTH_SURVEY.pdf">FEMALE HEALTH SURVEY.pdf</a>
+  <li><a href="forms/PS_Medical_History.pdf">Medical History.pdf</a>
+  <li><a href="forms/PS_Patient_Registration.pdf">Patient Registration.pdf</a>
+  <li><a href="forms/PS_Payment_Policy.pdf">Payment Policy.pdf</a>
+  <li><a href="forms/PS_Review_of_Systems.pdf">Review of Systems.pdf</a>
 </ul>
 
 <?php include('footer.php') ?>
