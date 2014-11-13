5462b8b2bcefbeade19b04ccb860d6aec7693c44
Karen Zink <unspecified>
2014-11-12 16:28:10 -0800

    Packet

--- forms.php
+++ forms.php
@@ -2,16 +2,24 @@
 
 <h2>Forms</h2>
 
-<p> Please download the following forms before your visit.  Having these filled out ahead of time will
-help streamline your visit.  If you would like us to mail or email the forms to you, contact our office.  
-Please fill out the Registration, Medical History, Review of Systems, Family Authorization, and Payment Policy.  If you are 
-female, please fill out the Female Health Survey.  The Privacy Notice is 
-the legal notice of our privacy practices. You need to sign the Privacy Acknowledgement, stating that you
-received our Privacy Notice.  Thank you.  We are looking forward to meeting you. </p>
+<p> Please download the New Patient Packet before your visit.  Having these 
+filled out ahead of time will help streamline your visit.  The HIPAA notice 
+is for your information.  
+You can choose whether or not you would like to print it.  
+If you would like us to mail or email the forms to you, contact our office.  
+</p>
+
+<p>
+Please fill out all of the forms.  If you are female, please fill out the 
+Female Health Survey.  Men do not need to fill out the "Female Health History."  
+The Privacy Notice is the legal notice of our privacy practices. You need to 
+sign the Privacy Acknowledgement, stating that you
+received our Privacy Notice (HIPAA notice).  Thank you.  We are looking 
+forward to meeting you. </p>
 
 <ul>
-  <li><a href="forms/PS_Patient_Registration.pdf">Registration.pdf</a>
-  <li><a href="forms/PS_Medical_History.pdf">Medical History.pdf</a>
+  <li><a href="forms/New_patient_packet.pdf">New Patient Packet.pdf</a>
+  <li><a href="forms/HIPAA_Notice.pdf">HIPAA Notice.pdf</a>
   <li><a href="forms/PS_Review_of_Systems.pdf">Review of Systems.pdf</a>  
   <li><a href="forms/Authorization.pdf">Family Authorization.pdf</a>
   <li><a href="forms/PS_FEMALE_HEALTH_SURVEY.pdf">Female Health Survey.pdf</a>
